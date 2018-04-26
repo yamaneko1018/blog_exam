6e7cc4d83234e47fa8bc0c9489e6eb680beeb359
yamaneko <yunisy1018@gmail.com>
2018-04-26 06:11:30 +0000

    完成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,26 +1,49 @@
 class BlogsController < ApplicationController
+    before_action :set_blog,only:[:show, :edit, :update, :destroy]
     
     def index
         @blogs = Blog.all
     end
     
     def new
-        @blog = Blog.new
+        if params[:back]
+         @blog = Blog.new(blog_params)
+        else
+         @blog = Blog.new
+        end
     end
     
     def create
-        Blog.create(blog_params)
-        redirect_to blogs_path
+        @blog = Blog.new(blog_params)
+        if @blog.save
+         redirect_to blogs_path, notice:"ブログを作成しました！"
+        else
+         render 'new'
+        end
+    end
+    
+    def confirm
+        @blog = Blog.new(blog_params)
+        render :new if @blog.invalid?
     end
     
     def show
-        @blog = Blog.find(params[:id])
     end
     
     def edit 
     end
     
+    def update
+        if @blog.update(blog_params)
+         redirect_to blogs_path,notice: "ブログを編集しました！"
+        else 
+         render 'edit'
+        end
+    end
+    
     def destroy
+        @blog.destroy
+        redirect_to blogs_path,notice: "ブログを削除しました！"
     end
     
     private
@@ -28,4 +51,7 @@ class BlogsController < ApplicationController
         params.require(:blog).permit(:title,:content)
     end
     
+    def set_blog
+        @blog = Blog.find(params[:id])
+    end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,3 @@
 class Blog < ApplicationRecord
+    validates :title, :content, length: {minimum: 1,maximum: 140},presence: true
 end
